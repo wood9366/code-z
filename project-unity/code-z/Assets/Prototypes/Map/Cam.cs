6aeb7d5509ec47173ad5cfb12844f0d342348c93
liyang <7325560@qq.com>
2024-09-10 23:52:22 +0800

    make camera move smoothly.

--- project-unity/code-z/Assets/Prototypes/Map/Cam.cs
+++ project-unity/code-z/Assets/Prototypes/Map/Cam.cs
@@ -5,15 +5,12 @@ namespace PT.Map
     public class Cam : MonoBehaviour
     {
         public Transform _target;
+        public float _move_speed = 1;
+        public Vector3 _target_offset = Vector3.zero;
 
         private void Update()
         {
-            var pos = transform.position;
-
-            pos.x = _target.position.x;
-            pos.y = _target.position.y;
-
-            transform.position = pos;
+            transform.position = Vector3.Lerp(transform.position, _target.position + _target_offset, _move_speed * Time.deltaTime);
         }
     }
 }
